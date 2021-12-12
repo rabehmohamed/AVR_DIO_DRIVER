386933855d6ea7dd72371fc148711836bf8d9454
rabehmohamed <81047812+rabehmohamed@users.noreply.github.com>
2021-12-12 13:40:23 +0200

    Add files via upload

---
+++ AVR_INTERFACING/ADC.h
@@ -0,0 +1,29 @@
+/*
+ * ADC.h
+ *
+ * Created: 10/28/2021 3:25:31 PM
+ *  Author: Mohamed Magdi
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+#include <avr/io.h>
+#define ADC0 0
+#define ADC1 1
+#define ADC2 2
+#define ADC3 3
+#define ADC4 4
+#define ADC5 5
+#define ADC6 6
+#define ADC7 7
+
+#include <avr/io.h>
+#include "STD_Types.h"
+#include "Bitwise.h"
+#include "Dio.h"
+void Adc_Init(void) ;
+void Adc_ChannelInit(uint8 ADC_Channel);
+void Adc_ReadChannel(uint8 ADC_Channel , uint16* Adc_Data) ;
+
+#endif /* ADC_H_ */
